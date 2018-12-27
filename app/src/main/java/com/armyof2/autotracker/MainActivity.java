8219c3431f4bea1b2cb5ea14e5ff02576875b07d
Fx3xAxRx <fxexaxrx@gmail.com>
2018-12-27 18:22:30 +0530

    donno wat to say.. a lot was done main activity modified signin activity created firebase connected etc.. etc..

--- app/src/main/java/com/armyof2/autotracker/MainActivity.java
+++ app/src/main/java/com/armyof2/autotracker/MainActivity.java
@@ -1,13 +1,262 @@
 package com.armyof2.autotracker;
 
+import android.app.ProgressDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+import static com.armyof2.autotracker.SignInActivity.userUid;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TRACK_ID;
+    private TextView ownID;
+    private EditText trackId;
+    private EditText phoneNo;
+    private Intent intent;
+    private Switch tSwitch;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private Toast mToast;
+    private ArrayList<String> TIDs;
+    private ProgressDialog progress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        adjustFontScale(getResources().getConfiguration());
+        adjustDisplayScale(getResources().getConfiguration());
         setContentView(R.layout.activity_main);
+
+        trackId = (EditText) findViewById(R.id.et_trackid);
+        ownID = (TextView) findViewById(R.id.tv_title2);
+        phoneNo = (EditText) findViewById(R.id.et_phoneno);
+        TIDs = new ArrayList<>();
+        tSwitch = (Switch) findViewById(R.id.switch1);
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference();
+        //intent = new Intent(this, TrackActivity.class);
+
+        progress = new ProgressDialog(this);
+        progress.setMessage("Connecting to Server...");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+        progress.setCanceledOnTouchOutside(false);
+
+        mToast = Toast.makeText(this, "", Toast.LENGTH_SHORT);
+        myRef.child(userUid);
+        ownID.setText("Your Tracking ID:" + userUid);
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getKey();
+                TIDs.add(value);
+                Log.d("TAG", "onChildAdded: " + TIDs);
+                if(dataSnapshot.getKey().equals(userUid)) {
+                    boolean val = dataSnapshot.child("Track Allow").getValue(Boolean.class);
+                    tSwitch.setChecked(val);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                if(dataSnapshot.getKey().equals(userUid)) {
+                    boolean val = dataSnapshot.child("Track Allow").getValue(Boolean.class);
+                    tSwitch.setChecked(val);
+                }
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        tSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                myRef.child(userUid).child("Track Allow").setValue(isChecked);
+            }
+
+        });
+    }
+
+    public void onTrackButtonClicked(View view) {
+        if (!isNetworkAvailable(this)) {
+            Toast.makeText(this, "Are you connected to the internet?", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (trackId.getText().toString().equals("")) {
+            Toast.makeText(this, "Please specify a tracking ID", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (phoneNo.getText().toString().equals("")) {
+            Toast.makeText(this, "Please specify a phone number", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progress.setMessage("Connecting to Target...");
+        progress.show();
+
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getKey();
+                if(!value.equals(trackId.getText().toString()))
+                    badToast();
+                else if(value.equals(trackId.getText().toString())&&Boolean.parseBoolean(dataSnapshot.child("Track Allow").getValue().toString()))
+                    sendSMS("TRACK " + userUid);
+                else
+                    someToast();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    public void sendSMS(String msg){
+        if(phoneNo.getText().toString().length()!=10) {
+            Toast.makeText(this, "Invalid phone no.", Toast.LENGTH_SHORT).show();
+            progress.hide();
+            return;
+        }
+        try {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(phoneNo.getText().toString(), null, msg, null, null);
+                progress.hide();
+        } catch (Exception ex) {
+                ex.printStackTrace();
+        }
+    }
+
+    public void copyToClipboard(View view){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", userUid);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(getApplicationContext(), "Tracking ID Copied",Toast.LENGTH_SHORT).show();
+    }
+
+    public void goodToast() {
+        mToast.setText("Server joined!");
+        progress.hide();
+        mToast.show();
+    }
+
+    public void someToast() {
+        mToast.setText("Target device's track state is set to false");
+        progress.hide();
+        mToast.show();
+    }
+
+    public void badToast() {
+        mToast.setText("Target device could not be found!");
+        progress.hide();
+        mToast.show();
+    }
+
+    public static boolean isNetworkAvailable(Context con) {
+        try {
+            ConnectivityManager cm = (ConnectivityManager) con
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+            if (networkInfo != null && networkInfo.isConnected()) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent in = new Intent(this, SignInActivity.class);
+        startActivity(in);
+        finish();
+    }
+
+    public void adjustFontScale(Configuration configuration) {
+        if (configuration != null && configuration.fontScale != 1.0) {
+            configuration.fontScale = (float) 1.0;
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+            WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+            wm.getDefaultDisplay().getMetrics(metrics);
+            metrics.scaledDensity = configuration.fontScale * metrics.density;
+            this.getResources().updateConfiguration(configuration, metrics);
+        }
+    }
+
+    public void adjustDisplayScale(Configuration configuration) {
+        if (configuration != null) {
+            Log.d("TAG", "adjustDisplayScale: " + configuration.densityDpi);
+            if (configuration.densityDpi >= 485)
+                configuration.densityDpi = 500;
+            else if (configuration.densityDpi >= 300)
+                configuration.densityDpi = 400;
+            else if (configuration.densityDpi >= 100)
+                configuration.densityDpi = 200;
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+            WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+            wm.getDefaultDisplay().getMetrics(metrics);
+            metrics.scaledDensity = configuration.densityDpi * metrics.density;
+            this.getResources().updateConfiguration(configuration, metrics);
+        }
     }
 }
---
+++ app/src/main/java/com/armyof2/autotracker/SignInActivity.java
@@ -0,0 +1,338 @@
+package com.armyof2.autotracker;
+
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int RC_SIGN_IN = 7001;
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+    private TextView mStatusTextView;
+    public static String userUid;
+    private TextView welcomeView;
+    private GoogleSignInAccount acc;
+    private String name0;
+    private Animation in;
+    private Animation out;
+    private ProgressDialog progress;
+    private Boolean exit = false;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        adjustFontScale( getResources().getConfiguration());
+        adjustDisplayScale( getResources().getConfiguration());
+        setContentView(R.layout.activity_signin);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        //progress
+        progress = new ProgressDialog(this);
+        progress.setMessage("Signing into Poll4Bunk...");
+        progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress.setIndeterminate(true);
+
+        // Views
+        mStatusTextView = findViewById(R.id.status);
+        welcomeView = findViewById(R.id.tv_welcome);
+
+        //anime
+        in = new AlphaAnimation(0.0f, 1.0f);
+        in.setDuration(1200);
+        out = new AlphaAnimation(1.0f, 0.0f);
+        out.setDuration(1200);
+
+
+        // Button listeners
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.sign_out_button).setOnClickListener(this);
+        findViewById(R.id.proceed_button).setOnClickListener(this);
+        Button signInButton = (Button) findViewById(R.id.sign_in_button);
+        Typeface Roboto_Thin = Typeface.createFromAsset(getAssets(),  "fonts/Roboto-Thin.ttf");
+        signInButton.setTypeface(Roboto_Thin,Typeface.BOLD);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("TAG", "Google sign in failed", e);
+                Toast.makeText(this, "Are you connected to the Internet?", Toast.LENGTH_SHORT).show();
+                updateUI(null);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
+        //showProgressDialog();
+        progress.show();
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG", "signInWithCredential:failure", task.getException());
+                            Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        //hideProgressDialog();
+                    }
+                });
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void signOut() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void revokeAccess() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google revoke access
+        mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
+                new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        updateUI(null);
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        //hideProgressDialog();
+        progress.hide();
+        if (user != null) {
+            mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
+            acc = GoogleSignIn.getLastSignedInAccount(this);
+            if (acc != null) {
+                name0 = acc.getDisplayName();
+                String[] strArray = name0.split(" ");
+                String cap = strArray[0].substring(0, 1).toUpperCase() + strArray[0].substring(1);
+                Typeface Roboto_Thin_Light_Italic = Typeface.createFromAsset(getAssets(),  "fonts/Roboto-ThinItalic.ttf");
+                welcomeView.setTypeface(Roboto_Thin_Light_Italic);
+                welcomeView.setText("Welcome, " + cap);
+                welcomeView.startAnimation(in);
+
+            }
+            userUid = user.getUid();
+
+            findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+
+        } else {
+            Typeface Roboto_Thin_Light = Typeface.createFromAsset(getAssets(),  "fonts/Roboto-ThinItalic.ttf");
+            mStatusTextView.setTypeface(Roboto_Thin_Light);
+            mStatusTextView.setText(R.string.signed_out);
+            userUid = null;
+
+            welcomeView.startAnimation(out);
+            out.setAnimationListener(new Animation.AnimationListener() {
+
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    welcomeView.setText("");
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+
+
+            });
+
+            findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
+            findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.sign_in_button) {
+            signIn();
+        } else if (i == R.id.sign_out_button) {
+            revokeAccess();
+        } else if (i == R.id.proceed_button) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.home:
+                Intent i = new Intent(this,SignInActivity.class);
+                startActivity(i);
+                finish();
+                return true;
+
+            case R.id.about:
+                i = new Intent(this,Info.class);
+                startActivity(i);
+                return true;
+
+            case R.id.clog:
+                i = new Intent(this, com.armyof2.poll4bunk.Log.class);
+                startActivity(i);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+*/
+    @Override
+    public void onBackPressed() {
+        if (exit) {
+            finish(); // finish activity
+        } else {
+            Toast.makeText(this, "Press Back again to Exit.",
+                    Toast.LENGTH_SHORT).show();
+            exit = true;
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    exit = false;
+                }
+            }, 3 * 1000);
+
+        }
+
+    }
+
+
+    public void adjustFontScale(Configuration configuration) {
+        if (configuration != null && configuration.fontScale != 1.0) {
+            configuration.fontScale = (float) 1.0;
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+            WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+            wm.getDefaultDisplay().getMetrics(metrics);
+            metrics.scaledDensity = configuration.fontScale * metrics.density;
+            this.getResources().updateConfiguration(configuration, metrics);
+        }
+    }
+
+    public void adjustDisplayScale(Configuration configuration) {
+        DisplayMetrics metrics2 = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getRealMetrics(metrics2);
+        Log.d("TAG", "adjustDisplayScale: " + metrics2.densityDpi);
+
+        if (configuration != null) {
+            Log.d("TAG", "adjustDisplayScale: " + configuration.densityDpi);
+            if(configuration.densityDpi >= 485)
+                configuration.densityDpi = 500;
+            else if(configuration.densityDpi >= 300)
+                configuration.densityDpi = 400;
+            else if(configuration.densityDpi >= 100)
+                configuration.densityDpi = 200;
+            DisplayMetrics metrics = getResources().getDisplayMetrics();
+            WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
+            wm.getDefaultDisplay().getMetrics(metrics);
+            metrics.scaledDensity = configuration.densityDpi * metrics.density;
+            this.getResources().updateConfiguration(configuration, metrics);
+        }
+    }
+}
+
